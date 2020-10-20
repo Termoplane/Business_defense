369ba3a7ea16f749f2070f59fe5208ef642fc3ad
Termoplan <alex223666@gmail.com>
2020-10-20 18:34:14 +0300

    aa

--- plugins/element.js
+++ plugins/element.js
@@ -6,6 +6,7 @@ import 'element-ui/packages/theme-chalk/src/notification.scss'
 import 'element-ui/packages/theme-chalk/src/icon.scss'
 import Message from 'element-ui/lib/message'
 import MessageBox from 'element-ui/lib/message-box'
+import Notification from 'element-ui/lib/notification'
 
 Vue.prototype.$alert = MessageBox.alert
 Vue.prototype.$confirm = MessageBox.confirm
